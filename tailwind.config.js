928867f714591449d46dc2ca44ae5d5b3db13066
0xMirim <95051992+0xMirim@users.noreply.github.com>
2022-10-20 15:55:30 +0300

    fix: Update style and config files to import values from web-lib
    Import css styles as well as config for eslint and tailwind from web-lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,46 +1,16 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
 const {join} = require('path');
-const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-	presets: [
-		require('./node_modules/@yearn-finance/web-lib/dist/tailwind.plugin.cjs'),
-		require('./node_modules/@yearn-finance/web-lib/dist/tailwind.config.cjs')
-	],
+	presets: [require('@yearn-finance/web-lib/tailwind.config.cjs')],
 	content: [
 		join(__dirname, 'pages', '**', '*.{js,jsx,ts,tsx}'),
 		join(__dirname, 'components', 'icons', '**', '*.{js,jsx,ts,tsx}'),
 		join(__dirname, 'components', '**', '*.{js,jsx,ts,tsx}'),
-		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'dist', 'layouts', '**', '*.js'),
-		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'dist', 'components', '**', '*.js'),
-		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'dist', 'contexts', '**', '*.js'),
-		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'dist', 'icons', '**', '*.js'),
-		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'dist', 'utils', '**', '*.js')
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				aeonik: ['Aeonik', ...defaultTheme.fontFamily.sans],
-				mono: ['Aeonik Mono', ...defaultTheme.fontFamily.mono]
-			},
-			height: {
-				'inherit': 'inherit'
-			},
-			fontSize: {
-				'3xl': ['32px', '40px'],
-				'8xl': ['88px', '104px']
-			},
-			spacing: {
-				'50': '12.5rem',
-				'66': '16.5rem'
-			},
-			width: {
-				'5xl': '1120px'
-			},
-			maxWidth: {
-				'5xl': '1120px'
-			}
-		}
-	},
-	plugins: []
+		join(__dirname, 'utils', '**', '*.{js,jsx,ts,tsx}'),
+		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'layouts', '**', '*.js'),
+		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'components', '**', '*.js'),
+		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'contexts', '**', '*.js'),
+		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'icons', '**', '*.js'),
+		join(__dirname, 'node_modules', '@yearn-finance', 'web-lib', 'utils', '**', '*.js')
+	]
 };
