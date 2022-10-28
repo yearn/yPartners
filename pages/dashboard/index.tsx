366d6a6954104c09cce49140b4666464c7b13f29
0xMirim <95051992+0xMirim@users.noreply.github.com>
2022-10-28 16:33:57 +0300

    feat: Add dynamic title and logo to the dashboard depending on active partner

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,16 +1,19 @@
-import	React, {ReactElement}		from	'react';
-import	Image						from	'next/image';
-import	{unstable_getServerSession}	from 	'next-auth/next';
-import	{Button}					from	'@yearn-finance/web-lib/components';
-import	{authOptions}				from	'pages/api/auth/[...nextauth]';
 
-function	Index({session}: any): ReactElement {
+import	React, {ReactElement}		from	'react';
+// import	{unstable_getServerSession}	from 	'next-auth/next';
+import	{Button}					from	'@yearn-finance/web-lib/components';
+// import	{authOptions}				from	'pages/api/auth/[...nextauth]';
+import {usePartner} from 'contexts/usePartner';
+
+function	Index(): ReactElement {
+	const	{partner, logo} = usePartner();
+
 	return (
 		<main>
-			<section aria-label={'hero'} className={'grid grid-cols-12 items-center mt-[85px] mb-28'}>
+			<section aria-label={'hero'} className={'grid grid-cols-12 mt-[85px] mb-28'}>
 				<div className={'col-span-12 md:col-span-8'}>
-					<h1 className={'mb-6 text-6xl text-neutral-900 md:text-8xl'}>{'Hello You!'}</h1>
-					<p className={'w-3/4 text-lg'}>{`Your ID is ${session.user.name} and your token expires the ${session.expires}`}</p>
+					<h1 className={'mb-2 text-6xl text-neutral-900 md:text-8xl'}>{partner}</h1>
+					<p className={'mb-20 w-3/4 text-neutral-500'}>{'Last updated October 28th 2022, 16:20'}</p>
 					<div className={'flex flex-row mt-4 space-x-4'}>
 						<Button className={'w-[200px]'}>
 							{'Hello'}
@@ -21,31 +24,27 @@ function	Index({session}: any): ReactElement {
 
 					</div>
 				</div>
-				<div className={'hidden col-span-4 md:block'}>
-					<Image
-						src={'/b2bmeme.svg'}
-						loading={'eager'}
-						width={420}
-						height={445}
-						alt={''} />
+
+				<div className={'hidden col-span-3 md:block'}>
+					{logo?.current}
 				</div>
 			</section>
 		</main>
 	);
 }
 
-export async function getServerSideProps(context: any): Promise<any> {
-	const session = await unstable_getServerSession(context.req, context.res, authOptions);
-	if (!session) {
-		return {
-			redirect: {
-				destination: '/',
-				permanent: false
-			}
-		};
-	}
+// export async function getServerSideProps(context: any): Promise<any> {
+// 	const session = await unstable_getServerSession(context.req, context.res, authOptions);
+// 	if (!session) {
+// 		return {
+// 			redirect: {
+// 				destination: '/',
+// 				permanent: false
+// 			}
+// 		};
+// 	}
   
-	return {props: {session}};
-}
+// 	return {props: {session}};
+// }
 
 export default Index;
