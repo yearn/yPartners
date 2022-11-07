cfa641a3fe28010146c85d337ca493e2f3ed1a99
0xMirim <95051992+0xMirim@users.noreply.github.com>
2022-11-07 12:15:28 +0200

    refactor: Chart UI and logic into separate component
    Reduces complexity of Overview component

---
+++ components/dashboard/Chart.tsx
@@ -0,0 +1,88 @@
+import	React, {ReactElement}		from	'react';
+import {Chevron} from '@yearn-finance/web-lib/icons';
+import {Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
+
+import CustomTooltip from './CustomTooltip';
+
+type TChartData = {
+	name: string,
+	WBTC: number,
+	USDC: number,
+	rsWBTC: string,
+	rsUSDC: string,
+};
+
+type TProps = {
+  children?: React.ReactNode,
+	className?: string,
+	title: string,
+	tooltipSymbol: string,
+	windowValue: number | undefined,
+	data: TChartData[]
+}
+
+function Chart(props: TProps): ReactElement {
+	const {title, tooltipSymbol, data} = props;
+
+	return (
+		<div className={props.className}>
+			<h2 className={'mt-10 text-lg font-semibold'}>{title}</h2>
+			<div className={'flex justify-start items-center mt-2 mb-4 h-[350px]'}>
+				<ResponsiveContainer width={'85%'} height={'100%'}>
+					<BarChart
+						width={500}
+						height={300}
+						data={data}
+						margin={{
+							top: 10,
+							right: 80,
+							left: 0,
+							bottom: 5
+						}}
+					>
+						<XAxis />
+						<YAxis />
+						<Tooltip content={<CustomTooltip symbol={tooltipSymbol} />}/>
+						<Bar
+							dataKey={'USDC'}
+							stackId={'a'}
+							fill={'#8884d8'} />
+						<Bar
+							dataKey={'WBTC'}
+							stackId={'a'}
+							fill={'#82ca9d'} />
+					</BarChart>
+				</ResponsiveContainer>
+
+				<div className={'flex flex-col justify-center items-center -mt-8 -ml-4 w-[15%] h-full'}>
+					<div className={'flex flex-row -mt-8 -ml-8'}>
+						<div className={'w-4 h-5 bg-[#8884d8]'} />
+						<div>
+							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'USDC'}</p>
+							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'Wrapper: 0x23a...089ca'}</p>
+						</div>
+					</div>
+
+					<div className={'flex flex-row mt-7 -ml-8' }>
+						<div className={'w-4 h-5 bg-[#82ca9d]'} />
+						<div>
+							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'WBTC'}</p>
+							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'Wrapper: 0x23a...089ca'}</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div className={'flex justify-center items-center ml-10 w-3/4' }>
+				<Chevron className={'cursor-pointer'}/>
+				<span className={'ml-10 text-neutral-500'}>{'May'}</span> 
+				<span className={'mx-10 font-semibold'}>{'June'}</span>
+				<span className={'mr-10 text-neutral-500'}>{'July'}</span>
+				<Chevron className={'rotate-180 cursor-pointer'} />
+			</div>
+		</div>
+	);
+}
+
+export default Chart;
--- components/dashboard/Overview.tsx
+++ components/dashboard/Overview.tsx
@@ -1,9 +1,7 @@
-import	React, {MouseEvent, ReactElement, useState}		from	'react';
+import	React, {MouseEvent, ReactElement, useEffect, useState}		from	'react';
 import	{Button}					from	'@yearn-finance/web-lib/components';
-import	{Chevron}					from	'@yearn-finance/web-lib/icons';
-import {Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts';
 
-import CustomTooltip from './CustomTooltip';
+import Chart from './Chart';
 import VaultDetails from './VaultDetails';
 
 type TChartData = {
@@ -38,6 +36,12 @@ function generateData(window = 29): TChartData[]{
 function	Overview(): ReactElement {
 	const [activeWindow, set_activeWindow] = useState('1 month');
 	const [windowValue, set_windowValue] = useState(29);
+	const [dummyData, set_dummyData] = useState(generateData());
+
+	useEffect((): void => {
+		const data = generateData(windowValue);
+		set_dummyData(data);
+	}, [windowValue]);
 
 	function handleWindowChange(e: MouseEvent<HTMLButtonElement>): void {
 		const {name, value} = e.currentTarget;
@@ -63,118 +67,18 @@ function	Overview(): ReactElement {
 
 			<VaultDetails/>
 
-			<h2 className={'mt-10 text-lg font-semibold'}>{'Fees Earned'}</h2>
-			<div className={'flex justify-start items-center mt-2 mb-4 h-[350px]'}>
-				<ResponsiveContainer width={'85%'} height={'100%'}>
-					<BarChart
-						width={500}
-						height={300}
-						data={generateData(windowValue)}
-						margin={{
-							top: 10,
-							right: 80,
-							left: 0,
-							bottom: 5
-						}}
-					>
-						<XAxis />
-						<YAxis />
-						<Tooltip content={<CustomTooltip symbol={'K'} />}/>
-						<Bar
-							dataKey={'USDC'}
-							stackId={'a'}
-							fill={'#8884d8'} />
-						<Bar
-							dataKey={'WBTC'}
-							stackId={'a'}
-							fill={'#82ca9d'} />
-					</BarChart>
-				</ResponsiveContainer>
+			<Chart
+				title={'Fees Earned'}
+				windowValue={windowValue}
+				data={dummyData}
+				tooltipSymbol={'K'}/>
 
-				<div className={'flex flex-col justify-center items-center -mt-8 -ml-4 w-[15%] h-full'}>
-					<div className={'flex flex-row -mt-8 -ml-8'}>
-						<div className={'w-4 h-5 bg-[#8884d8]'} />
-						<div>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'USDC'}</p>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'Wrapper: 0x23a...089ca'}</p>
-						</div>
-					</div>
-
-					<div className={'flex flex-row mt-7 -ml-8' }>
-						<div className={'w-4 h-5 bg-[#82ca9d]'} />
-						<div>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'WBTC'}</p>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'Wrapper: 0x23a...089ca'}</p>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-			<div className={'flex justify-center items-center ml-10 w-3/4' }>
-				<Chevron className={'cursor-pointer'}/>
-				<span className={'ml-10 text-neutral-500'}>{'May'}</span> 
-				<span className={'mx-10 font-semibold'}>{'June'}</span>
-				<span className={'mr-10 text-neutral-500'}>{'July'}</span>
-				<Chevron className={'rotate-180 cursor-pointer'} />
-			</div>
-
-
-			<h2 className={'mt-10 text-lg font-semibold'}>{'Revenue Shared'}</h2>
-			<div className={'flex justify-start items-center mt-2 mb-4 h-[350px]'}>
-				<ResponsiveContainer width={'85%'} height={'100%'}>
-					<BarChart
-						width={500}
-						height={300}
-						data={generateData(windowValue)}
-						margin={{
-							top: 10,
-							right: 80,
-							left: 0,
-							bottom: 5
-						}}
-					>
-						<XAxis />
-						<YAxis />
-						<Tooltip content={<CustomTooltip symbol={'%'} />}/>
-						<Bar
-							dataKey={'rsUSDC'}
-							stackId={'a'}
-							fill={'#8884d8'} />
-						<Bar
-							dataKey={'rsWBTC'}
-							stackId={'a'}
-							fill={'#82ca9d'} />
-					</BarChart>
-				</ResponsiveContainer>
-
-				<div className={'flex flex-col justify-center items-center -mt-8 -ml-4 w-[15%] h-full'}>
-					<div className={'flex flex-row -mt-8 -ml-8'}>
-						<div className={'w-4 h-5 bg-[#8884d8]'} />
-						<div>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'USDC'}</p>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'Wrapper: 0x23a...089ca'}</p>
-						</div>
-					</div>
-
-					<div className={'flex flex-row mt-7 -ml-8' }>
-						<div className={'w-4 h-5 bg-[#82ca9d]'} />
-						<div>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'WBTC'}</p>
-							<p className={'mt-2 ml-2 text-xs underline md:text-sm'}>{'Wrapper: 0x23a...089ca'}</p>
-						</div>
-					</div>
-
-				</div>
-			</div>
-
-			<div className={'flex justify-center items-center mb-20 w-3/4' }>
-				<Chevron className={'cursor-pointer'} />
-				<span className={'ml-10 text-neutral-500'}>{'April'}</span> 
-				<span className={'mx-10 font-semibold'}>{'May'}</span>
-				<span className={'mr-10 text-neutral-500'}>{'June'}</span>
-				<Chevron className={'rotate-180 cursor-pointer'} />
-			</div>
+			<Chart
+				className={'mb-20'}
+				title={'Revenue Shared'}
+				windowValue={windowValue}
+				data={dummyData}
+				tooltipSymbol={'%'}/>
 		</div>
 	);
 }
