d50345aff1c429a528f973501fdffa4598b20630
0xMirim <95051992+0xMirim@users.noreply.github.com>
2022-11-07 16:05:57 +0200

    fix: Resolve incorrect display name on tooltips

--- components/dashboard/CustomTooltip.tsx
+++ components/dashboard/CustomTooltip.tsx
@@ -8,11 +8,18 @@ type TTooltip = {
 };
 
 function ToolTip({active, symbol, payload}: TTooltip): ReactElement | null {
+	function formatName(name: string): string {
+		return name.length > 4 ? name.substring(name.length - 4) : name;
+	}
+
 	if (active && payload !== null) {
+		const name1 = formatName(payload[1].name);
+		const name0 = formatName(payload[0].name);
+
 		return (
 			<div className={'p-2 bg-good-ol-grey-300 rounded opacity-90'}>
-				<p><span className={'font-semibold'}>{'USDC: '}</span>{`${payload[0].value} ${symbol}`}</p>
-				<p><span className={'font-semibold'}>{'WBTC: '}</span>{`${payload[1].value} ${symbol}`}</p>
+				<p><span className={'font-semibold'}>{`${name1}: `}</span>{`${payload[1].value} ${symbol}`}</p>
+				<p><span className={'font-semibold'}>{`${name0}: `}</span>{`${payload[0].value} ${symbol}`}</p>
 			</div>
 		);
 	}
