cdb92af06df474b84f420e01c1d6fad96d510336
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-01-05 18:23:48 +0200

    feat: Add dynamic tvl and fees for each vault

--- components/dashboard/VaultDetails.tsx
+++ components/dashboard/VaultDetails.tsx
@@ -1,29 +1,33 @@
 import	React		from	'react';
+import {formatAmount} from '@yearn-finance/web-lib/utils/format.number';
 
 import type {ReactElement} from 'react';
+import type {TPartnerVault} from 'types/types';
 
 
-function	VaultDetails(): ReactElement {
+function	VaultDetails(props: {vault: TPartnerVault}): ReactElement {
+	const {balance, tvl} = props.vault;
+
 	return (
 		<div className={'my-20 flex w-[80%] justify-between bg-good-ol-grey-100'}>
 			<div>
 				<p>{'TVL'}</p>
-				<h1>{'$ 987, 125, 743. 68'}</h1>
+				<h1>{formatAmount(tvl)}</h1>
 			</div>
 
 			<div>
 				<p>{'Fees earned to date'}</p>
-				<h1>{'$ 654, 125, 743. 68'}</h1>
+				<h1>{formatAmount(balance)}</h1>
 			</div>
 
 			<div>
 				<p>{'Annual Yield'}</p>
-				<h1>{'6 %'}</h1>
+				<h1>{'- %'}</h1>
 			</div>
 
 			<div>
 				<p>{'Risk Score'}</p>
-				<h1>{'4'}</h1>
+				<h1>{'-'}</h1>
 			</div>
 		</div>
 	);
