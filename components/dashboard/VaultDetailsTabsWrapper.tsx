a1b54583384f0eb749097fb4557ecc28c2588802
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-01-30 14:01:25 +0300

    feat: Re-enable Overview tab on dashboards
    Setup logic to allow viewing of overview before beginning implementation of aggregate charts

--- components/dashboard/VaultDetailsTabsWrapper.tsx
+++ components/dashboard/VaultDetailsTabsWrapper.tsx
@@ -22,11 +22,12 @@ function	Tabs({selectedIndex, set_selectedIndex}: TProps): ReactElement {
 	return (
 		<>
 			<nav className={'hidden flex-row items-center space-x-10 md:flex'}>
-				<button disabled className={'cursor-not-allowed'}>
+				<button
+					onClick={(): void => set_selectedIndex(-1)}>
 					<p
 						title={'Overview'}
-						aria-selected={false}
-						className={'hover-fix tab !cursor-not-allowed'}>
+						aria-selected={selectedIndex === -1}
+						className={'hover-fix tab'}>
 						{'Overview'}
 					</p>
 				</button>
@@ -54,7 +55,7 @@ function	Tabs({selectedIndex, set_selectedIndex}: TProps): ReactElement {
 							<Listbox.Button
 								className={'flex h-10 w-40 flex-row items-center border-0 border-b-2 border-neutral-900 bg-neutral-100 p-0 font-bold focus:border-neutral-900 md:hidden'}>
 								<div className={'relative flex flex-row items-center'}>
-									{Object.values(vaults || [])[selectedIndex]?.token || 'Vaults'}
+									{Object.values(vaults || [])[selectedIndex]?.token || 'Overview'}
 								</div>
 								<div className={'absolute right-0'}>
 									<Chevron
@@ -71,6 +72,12 @@ function	Tabs({selectedIndex, set_selectedIndex}: TProps): ReactElement {
 								leaveFrom={'transform scale-100 opacity-100'}
 								leaveTo={'transform scale-95 opacity-0'}>
 								<Listbox.Options className={'yearn--listbox-menu'}>
+									<Listbox.Option
+										className={'yearn--listbox-menu-item'}
+										value={-1}>
+										{'Overview'}
+									</Listbox.Option>
+
 									{Object.values(vaults || []).map((vault, idx): ReactElement => (
 										<Listbox.Option
 											className={'yearn--listbox-menu-item'}
@@ -109,13 +116,16 @@ function	VaultDetailsTabsWrapper(props: {partnerID: string}): ReactElement {
 
 				<div className={'flex flex-row items-center justify-end space-x-2 pb-0 md:pb-4 md:last:space-x-4'}>
 					<a
+						className={ selectedIndex === -1 ? 'hidden' : ''}
 						href={`${getExplorerURL(vaultChainID)}/address/${vaultAddress}`}
 						target={'_blank'}
 						rel={'noopener noreferrer'}>
 						<span className={'sr-only'}>{'Open in explorer'}</span>
 						<IconLinkOut className={'h-5 w-5 cursor-alias text-neutral-600 transition-colors hover:text-neutral-900 md:h-6 md:w-6'} />
 					</a>
-					<button onClick={(): void => copyToClipboard(vaultAddress)}>
+					<button
+						onClick={(): void => copyToClipboard(vaultAddress)}
+						className={ selectedIndex === -1 ? 'hidden' : ''}>
 						<span className={'sr-only'}>{'Copy address'}</span>
 						<IconCopy className={'h-5 w-5 text-neutral-600 transition-colors hover:text-neutral-900 md:h-6 md:w-6'} />
 					</button>
