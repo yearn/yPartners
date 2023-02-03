53b27933f0b6ed462d97cbe6a7a7850a40081fbb
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-02-03 20:31:54 +0300

    fix: Switch to staging url to temporarily fix data availability issue

--- next.config.js
+++ next.config.js
@@ -48,7 +48,7 @@ module.exports = ({
 		ALCHEMY_KEY: process.env.ALCHEMY_KEY,
 		INFURA_KEY: process.env.INFURA_KEY,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		YVISION_BASE_URI: 'https://api.yearn.vision'
-		// YVISION_BASE_URI: 'https://api.staging.yearn.vision'
+		// YVISION_BASE_URI: 'https://api.yearn.vision'
+		YVISION_BASE_URI: 'https://api.staging.yearn.vision'
 	}
 });
