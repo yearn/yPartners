5133857aa9e91386be8e89d1629cc1b4e7c37cec
0xMirim <95051992+0xMirim@users.noreply.github.com>
2023-02-20 17:46:46 +0300

    feat: Add calculation of wrapper balance percentages
    Used for Wrapper balance distribution chart

--- components/graphs/OverviewChart.tsx
+++ components/graphs/OverviewChart.tsx
@@ -1,6 +1,7 @@
-import	React	from	'react';
+import	React, {useMemo}	from	'react';
 import Chart from 'components/charts/Chart';
 import {getTickInterval} from 'utils/b2b/Chart';
+import {formatAmount} from '@yearn-finance/web-lib/utils/format.number';
 
 import type {ReactElement} from 'react';
 import type {TChartBar} from 'types/chart';
@@ -21,14 +22,35 @@ const dummyLegendSingle = [
 function	OverviewChart(props: TOverviewChartProps): ReactElement {
 	const {wrapperTotals, balanceTVLs, windowValue, activeWindow} = props;
 
-	console.log(balanceTVLs);
+	const wrapperPercentages = useMemo((): TDict<TChartBar[]> => {
+		const _percentages: TDict<TChartBar[]> = {};
+
+		Object.keys(balanceTVLs).forEach((address): void => {
+			const asset = balanceTVLs[address];
+
+			const result = asset.map((dataPoint, i): TChartBar => {
+				const {balanceTVL} = dataPoint.data;
+				const {totalTVL} = wrapperTotals[i].data;
+				const _percent = totalTVL === 0 ? 0 : +formatAmount((balanceTVL / totalTVL) * 100, 0, 2);
+				
+				return ({...dataPoint, data: {percentage: _percent, balanceTVL, totalTVL}});
+			});
+
+			_percentages[address] = result;
+		});
+
+		return _percentages;
+
+	}, [wrapperTotals, balanceTVLs]);
+
+	console.log(wrapperPercentages);
 	
 	return (
 		<div className={'h-[400px]'}>
 			<Chart
 				title={'Aggregate Wrapper Balance (USD)'}
 				type={'composed'}
-				className={'mb-20'}
+				className={'mb-10'}
 				windowValue={windowValue}
 				data={wrapperTotals}
 				bars={[{name: 'data.totalTVL', fill: '#8884d8'}, {name: 'data.profitShare', fill: '#82ca9d'}]}
