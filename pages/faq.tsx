368e64d146357575d62c006b91bebdbb20706cc9
engn33r <engn33r@users.noreply.github.com>
2026-02-17 23:12:42 +0100

    Integrate wrapper (#195)
    * Add 3 easy steps on homepage
    
    * Upgrade dependencies
    
    * Update info on how to integrate

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -4,9 +4,9 @@ function	FAQPage(): ReactElement {
 	return (
 		<div className={'w-full bg-neutral-200 p-6'}>
 			<div className={'mx-auto max-w-screen-lg space-y-8 p-2 sm:p-4'}>
-			
+
 				<h1 className={'pb-6 text-3xl font-bold'}>{'Frequently Asked Questions'}</h1>
-				
+
 				<section className={'mt-12 rounded-lg border border-neutral-300 bg-white p-6 shadow-sm'}>
 					<div className={'space-y-6 text-neutral-700 text-base sm:text-lg'}>
 						<div>
@@ -24,23 +24,38 @@ function	FAQPage(): ReactElement {
 						<div>
 							<p className={'text-lg font-semibold text-neutral-900 sm:text-xl'}>{'What is required to onboard a new partner?'}</p>
 							<p>
-								{'Not much, we just need a way to identify the correct list of user addresses who deposit into Yearn vaults from your frontend. One way to do this is to deploy a '}
+								{'First, get in touch with us by filling the contact form. Then, configure your dApp to deposit to Yearn v3 vaults using our referral contract at 0x3744Df2673097d738aCaa3E463E6D638867757f2 on supported chains (mainnet, Base, Arbitrum, Katana). We will discuss which referral code to use for the deposit call. The code for this referral contract is '}
 								<a
 									className={'text-primary-600 underline'}
 									href={'https://github.com/yearn/Yearn-ERC4626-Router'}
 									rel={'noreferrer'}
 									target={'_blank'}>
-									{'Yearn 4626 Router contract'}
+									{'here'}
 								</a>
-								{' that acts as a passthrough to Yearn vaults, and this Router contract will be specific to your partnership. Currently, partnering with Yearn is not permissionless as there is no way to automatically claim fees without manual effort.'}
+								{' and you can find a demo frontend to test deposits with referrals '}
+								<a
+									className={'text-primary-600 underline'}
+									href={'https://referral-wrapper-demo.onrender.com/'}
+									rel={'noreferrer'}
+									target={'_blank'}>
+									{'here'}
+								</a>
+								{'. Lastly, we need to flip the switch by making the login page work for you so you can start tracking how many fees you earn over time.'}
 							</p>
 						</div>
 						<div>
-							<p className={'text-lg font-semibold text-neutral-900 sm:text-xl'}>{'What is the onchain address of the wrapper contract?'}</p>
+							<p className={'text-lg font-semibold text-neutral-900 sm:text-xl'}>{'What is the wrapper contract?'}</p>
 							<p>
-								{'The wrapper contract address is '}
+								{'The wrapper contract address is a way for Yearn v3 vaults to support referral codes. This is not a native feature of the v3 vault code, so a very minimalistic wrapper contract had to be built. It is only used for deposits, so users will withdraw directly from Yearn vaults as they normally would. The contract is located at '}
 								<code>{'0x3744Df2673097d738aCaa3E463E6D638867757f2'}</code>
-								{' on Ethereum mainnet, Base, Arbitrum, and Katana.'}
+								{' on Ethereum mainnet, Base, Arbitrum, and Katana. The code for it is stored '}
+								<a
+									className={'text-primary-600 underline'}
+									href={'https://referral-wrapper-demo.onrender.com/'}
+									rel={'noreferrer'}
+									target={'_blank'}>
+									{'here.'}
+								</a>
 							</p>
 						</div>
 						<div>
