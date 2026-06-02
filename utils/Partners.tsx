2e8ca8f0b9e2f89dd8915a07abf6ebb172cd6b56
engn33r <engn33r@users.noreply.github.com>
2026-06-02 00:00:00 +0000

    Fix one line

--- utils/Partners.tsx
+++ utils/Partners.tsx
@@ -103,7 +103,7 @@ const PARTNER_VAULT_CONFIG: TPartnerVaultConfig = {
 		747474: {
 			// Example: [toAddress('0xVaultAddress')]: [toAddress('0xDepositorAddress1'), ...]
 			[toAddress('0xBe53A109B494E5c9f97b9Cd39Fe969BE68BF6204')]: [
-				toAddress('0x3c5Aac016EF2F178e8699D6208796A2D67557fe2')
+				toAddress('0x8244F0746396E06bD26F68C00E9b48b70b771472')
 			]
 		}
 	},
