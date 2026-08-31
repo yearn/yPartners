4cd96f73e72d67f3cf9b0802075be3c308504019
engn33r <engn33r@users.noreply.github.com>
2026-08-31 00:00:00 +0000

    Remove dead code

--- utils/Chart.tsx
+++
@@ -1,9 +0,0 @@
-import {formatAmount} from 'lib/yearn/utils/format.number';
-
-export function formatXAxis(value: number): string {
-	return `${value+1}`;
-}
-
-export function formatYAxis(symbol: string, value: number): string {
-	return symbol === '%' ? `${value}${symbol}` : `${formatAmount(value, 0, 0)}`;
-}
