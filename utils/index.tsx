130910c0b04166108c66f7914b68e9a968d93386
engn33r <engn33r@users.noreply.github.com>
2026-09-18 16:04:55 +0200

    Upgrade eslint to 10.10.0, patch eslint-plugin-react for eslint 10 compatibility, fix new recommended-rule errors (#249)

--- utils/index.tsx
+++ utils/index.tsx
@@ -5,24 +5,3 @@ export const NETWORK_LABELS: {[key: number]: string} = {
 	747474: 'Katana'
 };
 
-export const NETWORK_CHAINID: { [key: string]: number } = {
-	'ETH': 1,
-	'BASE': 8453,
-	'ARB': 42161,
-	'KATANA': 747474
-};
-
-export function getExplorerURL(chainID: number): string {
-	switch (chainID) {
-		case 1:
-			return 'https://etherscan.io';
-		case 8453:
-			return 'https://basescan.org';
-		case 42161:
-			return 'https://arbiscan.io';
-		case 747474:
-			return 'https://katanascan.com';
-		default:
-			return ('https://etherscan.io');
-	}
-}
